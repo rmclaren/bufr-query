9e595b5a3bb91791f7f5ada456298e2b966e74f6
rmclaren <ronald.mclaren@noaa.gov>
2024-12-19 15:23:35 -0500

    Enhancements to Encoder Description (#43)
    * added support for adding global and dimension variables programatically
    
    * updated unit test for manual modifications of output data
    
    * slight test improvmeent
    
    * update docs
    
    * removed uneeded method

--- python/py_encoder_description.cpp
+++ python/py_encoder_description.cpp
@@ -6,6 +6,7 @@
 */
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <string>
 
@@ -24,5 +25,83 @@ void setupEncoderDescription(py::module& m)
         py::arg("name"),
         py::arg("source"),
         py::arg("units"),
-        py::arg("longName") = "", "");
+        py::arg("longName")="",
+        py::arg("coordinates")="",
+        py::arg("chunks")=std::vector<size_t>{},
+        py::arg("compressionLevel")=3,
+        "Add a variable to the description.")
+   .def("remove_variable", &Description::removeVariable,
+        py::arg("name"),
+        "Remove a variable from the description.")
+    .def("add_dimension",
+         static_cast<void (Description::*)(const std::string&,
+                                           const std::vector<std::string>&,
+                                           const std::string&)>(&Description::addDimension),
+         py::arg("name"),
+         py::arg("paths"),
+         py::arg("source") = "",
+         "Add a dimension to the description.")
+   .def("remove_dimension", &Description::removeDimension,
+        py::arg("name"),
+        "Remove a dimension from the description.")
+   .def("add_global", [](Description& self, const std::string& name, const py::object& value)
+   {
+      if (py::isinstance<py::str>(value))
+      {
+          self.addGlobal(name, value.cast<std::string>());
+      }
+      else if (py::isinstance<py::float_>(value))
+      {
+        self.addGlobal(name, value.cast<float>());
+      }
+      else if (py::isinstance<py::int_>(value))
+      {
+        self.addGlobal(name, value.cast<int>());
+      }
+      else if (py::isinstance<py::list>(value))
+      {
+        py::list values = value.cast<py::list>();
+        if (py::isinstance<py::str>(values[0]))
+        {
+          std::vector<std::string> vec;
+          for (auto v : values)
+          {
+            vec.push_back(v.cast<std::string>());
+          }
+          self.addGlobal(name, vec);
+        }
+        else if (py::isinstance<py::float_>(values[0]))
+        {
+          std::vector<float> vec;
+          for (auto v : values)
+          {
+            vec.push_back(v.cast<float>());
+          }
+          self.addGlobal(name, vec);
+        }
+        else if (py::isinstance<py::int_>(values[0]))
+        {
+          std::vector<int> vec;
+          for (auto v : values)
+          {
+            vec.push_back(v.cast<int>());
+          }
+          self.addGlobal(name, vec);
+        }
+        else
+        {
+          throw eckit::BadValue("Unsupported data type encountered.");
+        }
+      }
+      else
+      {
+        throw eckit::BadValue("Unsupported data type encountered.");
+      }
+     },
+      py::arg("name"),
+      py::arg("value"),
+     "Add a global attribute to the description.")
+   .def("remove_global", &Description::removeGlobal,
+        py::arg("name"),
+        "Remove a global attribute from the description.");
 }
